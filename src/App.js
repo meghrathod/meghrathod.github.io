48418b9fb2bf3bd2422121c08a65c11a7a7a9346
Megh Rathod <msr.megh@gmail.com>
2021-05-08 15:13:27 +0530

    added meta using helmet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
-import Header from './components/Header/Header'
-import Body from './components/Body/Body.jsx'
-import Footer from './components/Footer/Footer'
+import "./App.css";
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body.jsx";
+import Footer from "./components/Footer/Footer";
+import Helmet from "helmet";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Body />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Megh Rathod is a student developer working towards becoming a great developer and give the world something amazing!"
+                />
+            </Helmet>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
