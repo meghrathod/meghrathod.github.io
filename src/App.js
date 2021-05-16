2245d3ffcb5398cbb02805add2e85bf35d668320
Megh Rathod <msr.megh@gmail.com>
2021-05-16 15:11:55 +0530

    added animated gradient background

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     return (
         <div className="App">
             <div className="background"></div>
+            <div className="background-image"></div>
             <Helmet>
                 <meta
                     name="description"
