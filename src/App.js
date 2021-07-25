dabdeaab73ac92c453a461bfb0033a2b47c60752
Megh Rathod <msr.megh@gmail.com>
2021-07-25 15:48:04 +0530

    Changed a few meta tags

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body.jsx";
 import Footer from "./components/Footer/Footer";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 
 function App() {
     return (
         <div className="App">
-            <div className="background"></div>
-            <div className="background-image"></div>
             <Helmet>
                 <meta
                     name="description"
@@ -18,9 +16,15 @@ function App() {
                     property="og:description"
                     content="I am a student developer trying and learning new technolgies everyday! I love all tech!"
                 />
+
                 <meta property="og:title" content="Megh Rathod" />
-                <meta property="og:image" content="./components/images/megh.png" />
+                <meta
+                    property="og:image"
+                    content="https://pbs.twimg.com/profile_images/1362374551548563463/-NFBY4tq_400x400.jpg"
+                />
             </Helmet>
+            <div className="background"></div>
+            <div className="background-image"></div>
             <Header />
             <Body />
             <Footer />
