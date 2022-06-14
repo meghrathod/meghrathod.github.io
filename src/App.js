056d3d6e53d6ce27ac6e1b8f2f108f7adb5aaa29
Megh Rathod <me@meghrathod.dev>
2022-06-14 17:19:03 +0530

    add: new animated background

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Body from "./components/Body/Body.jsx";
 import Footer from "./components/Footer/Footer";
 import { Helmet } from "react-helmet";
+import Background from "./components/Body/Background";
 
 function App() {
     return (
@@ -13,8 +14,7 @@ function App() {
                     content="Hey, I'm Megh Rathod, trying my hands at object oriented programming in Java, web development technologies, and also Machine Learning."
                 />
             </Helmet>
-            <div className="background"></div>
-            <div className="background-image"></div>
+            <Background />
             <Header />
             <Body />
             <Footer />
---
+++ src/components/Body/Background.jsx
@@ -0,0 +1,24 @@
+import "./background.scss";
+
+function Background() {
+    return (
+        <div className="background">
+            <ul className="circles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
+
+
+}
+
+export default Background;
