251b0a3127031da147995faad34dec5d6ba62cbc
Megh Rathod <me@meghrathod.dev>
2022-08-22 18:41:59 +0530

    fix: consistency in function definition
    Signed-off-by: Megh Rathod <me@meghrathod.dev>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.scss";
 import React from "react";
 
 // Footer component
-const Footer = () => {
+function Footer () {
     return (
         <div className="footer">
             &copy; {new Date().getFullYear()} Megh Rathod
